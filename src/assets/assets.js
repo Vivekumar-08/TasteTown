2e08fc28233e658d0cd55337d2507dead3a3993d
Vivekumar-08 <vk8584686@gmail.com>
2025-01-06 20:08:41 +0530

    Add menu components & Updated page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,121 +1,133 @@
 import menu__1 from './menu__1.jpg'
 import menu__2 from './menu__2.jpg'
+import menu__3 from './menu__3.jpg'
+import menu__4 from './menu__4.jpg'
+import menu__5 from './menu__5.jpg'
+import menu__6 from './menu__6.jpg'
 import menu__7 from './menu__7.jpg'
+import menu__8 from './menu__8.jpg'
+import menu__9 from './menu__9.jpg'
+import menu__10 from './menu__10.jpg'
+import menu__11 from './menu__11.jpg' 
+import menu__13 from './menu__13.jpg'
+import menu__14 from './menu__14.jpg'
+import menu__15 from './menu__15.jpg'
+import menu__16 from './menu__16.jpg'
+import menu__17 from './menu__17.jpg'
+import menu__18 from './menu__18.jpg'
+import menu__19 from './menu__19.jpg'
+import menu__20 from './menu__20.jpg'
+import menu__21 from './menu__21.jpg'
+import menu__22 from './menu__22.jpg'
+import menu__23 from './menu__23.jpg'
+
 export const menu__list = [
     {
-        menu__name: "Salad",
+        menu__name: "Burger",
         menu__image: menu__1
     },
     {
-        menu__name: "Salad",
+        menu__name: "Biryani",
         menu__image: menu__2
     },
     {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__name: "Pizza",
+        menu__image: menu__3
     },
     {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__name: "Noodles",
+        menu__image: menu__4
     },
     {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__name: "Chole Bhature",
+        menu__image: menu__5
     },
     {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__name: "Pasta",
+        menu__image: menu__6
     },
     {
-        menu__name: "Salad",
+        menu__name: "Cake",
         menu__image: menu__7
     },
     {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__name: "Paratha",
+        menu__image: menu__8
+    },
+    {
+        menu__name: "Pure Veg",
+        menu__image: menu__9
     },
     {
         menu__name: "Salad",
-        menu__image: menu__1
+        menu__image: menu__10
+    },
+    {
+        menu__name: "Shawaarma",
+        menu__image: menu__11
+    },
+    {
+        menu__name: "Waffle",
+        menu__image: menu__17
     },
     {
         menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
-    },
-    {
-        menu__name: "Salad",
-        menu__image: menu__1
+        menu__image: menu__15
     },
 ]
 
+
 export const menu__list__01 = [
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Dosa",
+        menu__image: menu__13
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Momos",
+        menu__image: menu__14
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Pastry",
+        menu__image: menu__15
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Rolls",
+        menu__image: menu__16
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "ice-cream",
+        menu__image: menu__17
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Vada",
+        menu__image: menu__18
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Kachori",
+        menu__image: menu__19
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Pakoda",
+        menu__image: menu__20
     },
     {
-        menu__name: "Salad",
-         menu__image: menu__1
+        menu__name: "Samosa",
+        menu__image: menu__21
     },
-
+    {
+        menu__name: "Shake",
+        menu__image: menu__22
+    },
+    {
+        menu__name: "Pav Bhaji",
+        menu__image: menu__23
+    },
+    {
+        menu__name: "Chole Bhature",
+        menu__image: menu__5
+    },
+    {
+        menu__name: "Noodles",
+        menu__image: menu__4
+    },
 ]
